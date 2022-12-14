ce27976ca6cb2943607e01af2b60d463bcdc49cf
Aviv Maman <awiwmaman@outlook.com>
2022-12-14 17:20:37 +0200

    CHG: addDoc was added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import db from '../firebase/firebaseConfig';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
 
 export type Book = {
   id: string;
@@ -27,22 +27,22 @@ export type Query = {
 export type ApiClient = {
   getBooks: (query?: Query) => Promise<Book[]> | PromiseLike<Book[]> | unknown;
   getBookById: (bookId: string) => Promise<Book>;
-  createBook: (formData: any) => Promise<Book>;
+  addBook: (formData: any) => Promise<any>;
 };
 
 export const createApiClient = (): ApiClient => {
+  const collectionRef = collection(db, 'books');
   return {
     getBooks: async (queryObject?: Query) => {
       const controller = new AbortController();
       try {
         // const wantedQuery = query(ref, where('title', '==', queryObject?.q));
-        const collectionRef = collection(db, 'books');
         const snapshot = await getDocs(collectionRef);
         const books = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as Book));
         return books;
       } catch (error) {
         if (controller.signal.aborted) {
-          console.log('the request was cancelled:', controller.signal.reason);
+          console.log('The request was cancelled:', controller.signal.reason);
         } else {
           console.log('Could not fetch the data.');
           return error;
@@ -56,9 +56,20 @@ export const createApiClient = (): ApiClient => {
       });
       return res.data;
     },
-    createBook: async (formData) => {
-      const res = await axios.post(`http://localhost:5000/books`, formData);
-      return res.data;
+    addBook: async (formData) => {
+      const controller = new AbortController();
+      try {
+        const res = await addDoc(collectionRef, formData);
+        return { message: 'A document was successfully added.', id: res.id };
+      } catch (error) {
+        if (controller.signal.aborted) {
+          console.log('the request was cancelled:', controller.signal.reason);
+        } else {
+          console.log('There was a problem with adding the document.');
+          return error;
+        }
+      }
+      return () => controller.abort();
     },
   };
 };
--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
+import { createApiClient } from '../api/api';
+
+//API calls
+const api = createApiClient();
 
 export default function BookForm() {
   const [newBook, setNewBook] = useState('');
+  const [serverResponse, setServerResponse] = useState({ message: '' });
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    console.log(newBook);
-
+    const res = await api.addBook({ title: newBook });
     setNewBook('');
+    setServerResponse({ message: res.message });
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {serverResponse?.message?.length > 0 ? <p>{serverResponse.message}</p> : null}
       <label>
         <span>Add a new book title:</span>
         <input required type='text' onChange={(e) => setNewBook(e.target.value)} value={newBook} />
